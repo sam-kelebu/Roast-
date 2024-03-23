8ce6cadb1e7a4d0fdd503333810679a85f7096e1
samual keleb <samualkelebu@gmail.com>
2024-03-24 01:12:24 +0300

    second commit router set

--- roaste/src/App.jsx
+++ roaste/src/App.jsx
@@ -1,15 +1,32 @@
 import react from "react";
 import Routing from "./Routing";
 // import MainPage from './Component/MainPage'
-// import MainPage from "./Component/MainPage";
+import Female from "./Component/Female"
+import Male from "./Component/Men"
+import MainPage from "./Component/MainPage";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { BrowserRouter,Routes,Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Routing />
-      <Outlet />
+      <BrowserRouter>
+      <nav className="flex justify-between  bg-red-400 w-full fixed top-0">
+      <Link to="/">Roster</Link>
+      <div className="">
+      <Link to="/Contact">Contact</Link>
+      <Link to="/Mywork">My-work</Link>
+      </div>
+      </nav>
+      <Routes>
+        <Route index  element={<MainPage/>}/>
+        <Route path="/Male" element={<Male/>}/>
+        <Route path="/Female" element={<Female/>}/>
+       
+      </Routes>
+      </BrowserRouter>
+      
+      {/* <Outlet /> */}
       {/* <MainPage /> */}
       <Footer />
     </div>
